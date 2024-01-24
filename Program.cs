12b495ee3d8b60acf5b4379ed004c2c9610e0bac
alexa <alexa@ALEXANDRAS-LAPT>
2024-01-24 12:21:43 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// MIS 3013 001
+// Jan 24, 2024
+// Alexandra Camarena
+// 113568154
+
+// define variable
+// int double bool char
+// int : 1 2 3 -1 0 
+// double : 12.6
+// bool : true false
+// char : one single character
+// datatype varaible_name;
+// 1. letter number _
+// 2. start with letter or _ (could not start with a number)
+// int double bool char: simple datatype, bot expensive resources in the computer
+int age1;// datatype is int, angel is the varaible
+int age2;//
+
+double weight1;
+double weight2;
+
+bool b1;
+bool b2;
+
+char c1;
+char c2;
+// to put values into the space, we use assingment operation
+// = assignment operator, very very important concept in prgramming
+// variable_name = expression;
+age1 = 20;
+age2 = 2 * 15;
+
+weight1 = age1 * 10;
+
+Console.WriteLine(weight1);
+Console.WriteLine(age2);
